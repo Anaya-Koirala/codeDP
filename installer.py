a7892fd5cbe8dc6f17884108ef3e3cfa0734c3c0
Anaya Koirala <anayakoirala@protonmail.com>
2023-12-18 21:05:54 +0545

    Update installer.py

--- installer.py
+++ installer.py
@@ -3,15 +3,12 @@ from PyInstaller.__main__ import run
 
 if __name__ == "__main__":
     options = [
-        "--onefile",  # Create a single executable file
-        "--name=codeDP",  # Name of the executable file
-        "--icon=ui/icons/icon.svg",  # Set icon
+        "--onefile",  # Single executable file
+        "--name=codeDP",  # Name
+        "--icon=ui/icons/icon.svg",  # Icon
         "--hidden-import=ibdp_classes",  # Manage Import
-        "--windowed",
+        "--windowed", # Windowed mode without the console
     ]
 
-    # Add your main script
     scripts = ["editor.py"]
-
-    # Run PyInstaller
     sys.exit(run(options + scripts))
